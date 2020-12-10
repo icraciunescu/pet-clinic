786e1dd777dc3049cbdf3c3ae59a7b3e21667984
icraciunescu <icraciunescu@hotmail.com>
2020-12-10 21:23:29 +0200

    Refactoring to multi module Maven project. Closes #1

---
+++ pet-clinic-web/src/test/java/ro/mediaxpress/petclinic/PetClinicApplicationTest.java
@@ -0,0 +1,14 @@
+package ro.mediaxpress.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class PetClinicApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
