f2ce058869c313bf9e252c825e4908172ddc1b55
icraciunescu <icraciunescu@hotmail.com>
2020-12-12 08:40:45 +0200

    Create Interface for Pet Clinic Service. Closes #5

---
+++ pet-clinic-data/src/main/java/ro/mediaxpress/petclinic/services/PetService.java
@@ -0,0 +1,13 @@
+package ro.mediaxpress.petclinic.services;
+
+import ro.mediaxpress.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+
+}
---
+++ pet-clinic-data/src/main/java/ro/mediaxpress/petclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package ro.mediaxpress.petclinic.services;
+
+import ro.mediaxpress.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+
+}
